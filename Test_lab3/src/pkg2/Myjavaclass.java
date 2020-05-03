b75c781b61198c4f8b1262e92af07aa09c113c81
Animesh9 <aniajrip@gmail.com>
2020-05-04 00:30:06 +0530

    pkg2

---
+++ Test_lab3/src/pkg2/Myjavaclass.java
@@ -0,0 +1,14 @@
+package pkg2;
+import pkg1.*;
+public class Myjavaclass {
+
+	public static void main(String[] args) {
+		MyClass1 ob1 = new MyClass1();
+		MyClass2 ob2 = new MyClass2();
+		System.out.println("sum = " +ob1.sum(10,5));
+		System.out.println("sub = " +ob1.sub(10,5));
+		System.out.println("mul = " +ob2.mul(10,5));
+		System.out.println("div = " +ob2.div(10,5));
+	}
+
+}
